4e0c91bc1e3e0a63d09ab6df008d544de1be8ef0
Lance R. Vick <lance@lrvick.net>
2011-03-16 16:54:15 -0400

    some refactoring and improved testing

--- synt.py
+++ synt.py
@@ -8,9 +8,11 @@ from nltk.collocations import BigramCollocationFinder
 
 db_file = 'sample_data.db'
 
-classifier_file = 'classifier.gz'
+classifier_file = 'classifier.pkl'
 
 use_redis = False
+
+use_gzip = False
 
 def db_init():
     if not os.path.exists(db_file):
@@ -30,11 +32,11 @@ def gen_bow(text):
         nltk.download('stopwords')
     stemmer = PorterStemmer()
     tokenizer = WordPunctTokenizer()
-    tokens =  [stemmer.stem(x.lower()) for x in tokenizer.tokenize(text) if x not in stopwords.words('english') and len(x) > 1]
+    tokens = set(stemmer.stem(x.lower()) for x in tokenizer.tokenize(text)) - set(stopwords.words('english')) - set('')
     bag_of_words = dict([(token, True) for token in tokens])
     return bag_of_words
 
-def get_training_limit(): 
+def get_training_limit():
     db = db_init()
     db.execute("SELECT COUNT(*) FROM item where sentiment = 'positive'")
     pos_count = db.fetchone()[0]
@@ -46,72 +48,93 @@ def get_training_limit():
         limit = neg_count
     return limit
 
-
-def gen_classifier():
+def get_samples(limit=None):
     db = db_init()
-    limit = get_training_limit()
+    if not limit:
+        limit = get_training_limit()
+    else:
+        limit = (int(limit)/2)
     db.execute("SELECT * FROM item where sentiment = 'positive' LIMIT ?",[limit])
     samples = db.fetchall()
     db.execute("SELECT * FROM item where sentiment = 'negative' LIMIT ?",[limit])
     samples += db.fetchall()
+    return samples
+
+def gen_classifier_dict(samples=None):
+    if not samples:
+        samples = get_samples()
     sample_num = len(samples)
-    train_feats = []
-    total_samples = limit*2
+    classifier_dict = []
+    total_samples = len(samples)
     processed_samples = 0
     for sample in samples:
         percent = int(processed_samples*100/total_samples)
-        sys.stdout.write("\rGenerating Classifier - Samples: %s/%s - %d%%" % (processed_samples,total_samples,percent))
-        sys.stdout.flush()
         processed_samples += 1
+        sys.stdout.write("\rGenerating Classifier Dict - Samples: %s/%s - %d%%\r" % (processed_samples,total_samples,percent))
+        sys.stdout.flush()
         text = sample[2]
         sentiment = sample[4]
         bag_of_words = gen_bow(text)
         if bag_of_words and sentiment:
-            train_feats.append((bag_of_words,sentiment))
-    classifier = NaiveBayesClassifier.train(train_feats)
-    print ('saving classifier')
-    fp = gzip.open(classifier_file,'wb')
+            classifier_dict.append((bag_of_words,sentiment))
+    sys.stdout.write("\rGenerating Classifier Dict - Samples: %s/%s - 100%%\r" % (processed_samples,total_samples))
+    sys.stdout.write("\n\r")
+    return classifier_dict
+
+def gen_classifier():
+    classifier_data = gen_classifier_dict()
+    classifier = NaiveBayesClassifier.train(classifier_data)
+    print("Saving classifier to disk as: %s" % classifier_file)
+    if use_gzip == True:
+        fp = gzip.open(classifier_file,'wb')
+    else:
+        fp = open(classifier_file,'wb')
     cPickle.dump(classifier,fp)
     fp.close()
     return classifier
 
 def get_classifier(generate=False,use_redis=False):
     db = db_init()
+    classifier = False
     if generate == True:
         classifier = gen_classifier()
     elif not os.path.exists(classifier_file):
         classifier = gen_classifier()
-    if use_redis == True:
-        if cache.get('synt_class'):
-            print ('loading classifier from Redis cache')
-            classifier = cPickle.loads(cache.get('synt_class'))
     else:
-        print ('loading classifier from file')
-        fp = gzip.open(classifier_file,'rb')
-        classifier = cPickle.load(fp)
-        fp.close()
-        print ('saving classifier to Redis cache')
         if use_redis == True:
-            cache.set('synt_class',cPickle.dumps(classifier))
+            if cache.get('synt_class'):
+                print ('Loading Classifier from Redis cache')
+                classifier = cPickle.loads(cache.get('synt_class'))
+        if not classifier: 
+            print ('Loading Classifier from file: %s' % classifier_file)
+            if use_gzip == True:
+                fp = gzip.open(classifier_file,'rb')
+            else:
+                fp = open(classifier_file,'rw')
+            classifier = cPickle.load(fp)
+            fp.close()
+            if use_redis == True:
+                print ('saving classifier to Redis cache')
+                cache.set('synt_class',cPickle.dumps(classifier))
     return classifier
 
 def guess(text,classifier=None):
     if not classifier:
         classifier = get_classifier()
-    print ('classifier loaded')
     bag_of_words = gen_bow('text')
     return classifier.classify(bag_of_words)
 
-def test():
-    db = db_init()
-    classifier = get_classifier()
+def test(num_samples=None):
+    if not num_samples:
+        num_samples = 1000
     results_dict = []
-    total_accuracy = 0
-    db.execute("SELECT * FROM item LIMIT 100")
-    samples = db.fetchall()
+    accurate_samples = 0
+    classifier = get_classifier()
+    samples = get_samples(num_samples)
+    nltk_testing_dict = gen_classifier_dict(samples)
+    nltk_accuracy = nltk.classify.util.accuracy(classifier,nltk_testing_dict) * 100
     for sample in samples:
         text = sample[2]
-        print sample
         synt_guess = guess(text,classifier)
         known = sample[4]
         if known == synt_guess:
@@ -122,9 +145,11 @@ def test():
     for result in results_dict:
         print ("Text: %s" % (result[3]))
         print ("Accuracy: %s | Known Sentiment: %s | Guessed Sentiment: %s " % (result[0],result[1],result[2]))
-        print ("----------------------------------------------------------------------------------------------")
+        print ("------------------------------------------------------------------------------------------------------------------------------------------")
         if result[0] == True:
-            total_accuracy += 1
-    print("Total classifier accuracy = %s%%" % total_accuracy) 
+            accurate_samples += 1
+        total_accuracy = accurate_samples*100/num_samples
+    print("\n\rManual classifier accuracy result: %s%%" % total_accuracy) 
+    print('\n\rNLTK classifier accuracy result: %.2f%%' % nltk_accuracy)
 
 test()
