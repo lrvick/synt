509745b9cd1f18aa98a03471d8212c13a96379e5
Titus Soporan <titus@tsoporan.com>
2011-09-12 16:03:59 -0400

    collect.py returns pos/neg tweets.

--- utils/collect.py
+++ utils/collect.py
@@ -6,39 +6,69 @@ import sqlite3
 from synt.utils.db import db_init
 from synt.utils.text import sanitize_text
 
-def twitter_feed(sentiment, last_id=None, **kwargs):
+def twitter_feed(sentiment, last_id=None, new=False):
+    """
+    Will return positive and negative tweets from the twitter api.
+
+    What is flagged as positive and negative iscurrently determined
+    by happy/sad faces.
+    """
+    
     db = db_init()
     cursor = db.cursor()
-    if sentiment == 'positive':
+
+    if sentiment == 'positive': 
         query = ':)'
-    if sentiment == 'negative':
+    elif sentiment == 'negative':
         query = ':('
-    if not last_id:
-        cursor.execute('SELECT item_id FROM item WHERE sentiment=? ORDER BY item_id DESC LIMIT 1',[sentiment])
+    else:
+        print('Sentiment must be either positive or negative.')
+        return
+
+    last_id_url = "http://search.twitter.com/search.json?lang=en&q=%s&since_id=%s"
+    query_url   = "http://search.twitter.com/search.json?lang=en&q=%s" 
+
+    if not (last_id or new):
+        cursor.execute('SELECT item_id FROM item WHERE sentiment=? ORDER BY item_id DESC LIMIT 1', [sentiment])
         last_id = cursor.fetchone()[0]
-    url = "http://search.twitter.com/search.json?lang=en&q=%s&since_id=%s" % (query,last_id)
+        url = last_id_url  % (query, last_id)
+    elif last_id:
+        url = last_id_url  % (query, last_id)
+    elif new:
+        url = query_url % query
+    
+    print(url) 
+
+    data = []
+
     try:
         data = json.loads(urllib2.urlopen(url).read())
-    except Exception,e:
-        data = None
-        pass
+    except:
+        raise
+    
     if data:
+        
         items = data['results']
         cursor.execute('SELECT COUNT() FROM item WHERE sentiment = ?',[sentiment])
         total_rows = cursor.fetchone()[0]
+        
         for item in items:
-            text = item['text'].encode('utf8')
-            formatted_text = sanitize_text(text)
-            if formatted_text:
+            
+            text = unicode(item['text']) #force unicode for db
+            
+            if text:
+                
                 item_id = item['id']
+                
                 try:
-                    cursor.execute('INSERT INTO item VALUES (NULL,?,?,?,?)',[item_id,formatted_text,text,sentiment])
+                    cursor.execute('INSERT INTO item VALUES (NULL,?,?,?)', [item_id, text, sentiment])
                     last_id = item_id
-                    print sentiment,total_rows,text
+                    print sentiment, total_rows, text
                     total_rows += 1
-                except sqlite3.IntegrityError,e:
-                    pass
+                except sqlite3.IntegrityError, e:
+                    pass #these are duplicates, we don't want duplicates
+
     db.commit()
     db.close()
-    return last_id
 
+    return last_id
