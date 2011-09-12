73221f6b4093fa78ca13d42fb2a5ebf92f7d5992
Titus Soporan <titus@tsoporan.com>
2011-09-12 16:04:07 -0400

    collector now handles not having a sample_db

--- collector.py
+++ collector.py
@@ -1,13 +1,29 @@
 from synt.utils.collect import twitter_feed
+import time
+import os
+import settings
 
 def collect():
-    """Will continue populating sample database with content."""
+    """
+    Will continuously populate the sample database if it exists
+    else it will create a new one.
+    """
     
+
     neg_lastid, pos_lastid = None, None
+    
+    if not os.path.exists(settings.DB_FILE):
+        pos_lastid = twitter_feed('positive', pos_lastid, new=True)
+        neg_lastid = twitter_feed('negative', neg_lastid, new=True)
+
 
     while True:
         time.sleep(1)
-        pos_lastid = twitter_feed('positive', pos_lastid)
-        neg_lastid = twitter_feed('negative', neg_lastid)
+        try:
+            pos_lastid = twitter_feed('positive', pos_lastid)
+            neg_lastid = twitter_feed('negative', neg_lastid)
+        except:
+            raise
 
-
+if __name__ == '__main__':
+    collect()
