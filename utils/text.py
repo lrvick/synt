5e119c964123b1d7f8ee4bfa32325f3ae7c1e4a1
Titus Soporan <titus@tsoporan.com>
2011-09-13 19:25:38 -0400

    sanitize_text supports unicode, removal of BeautifulSoup

--- utils/text.py
+++ utils/text.py
@@ -3,46 +3,40 @@
 import re
 import string
 import synt.settings as settings
-from BeautifulSoup import BeautifulSoup
 from nltk.tokenize import WhitespaceTokenizer
+
+PUNC_MAP = dict([(ord(x),None) for x in string.punctuation]) 
 
 def sanitize_text(text):
     """
     Formats text to strip unneccesary:words, punctuation and whitespace. Returns a tokenized list.
-    
-    >>> text = "ommmmmmmg how'r u!? visi t   my site @ http://www.coolstuff.com"
+   
+    >>> text = u"ommmmmmg how'r u!? visi t  <html> <a href='http://google.com'> my</a> site @ http://www.coolstuff.com"
     >>> sanitize_text(text)
-    ['ommg', 'howr', 'visi', 'my', 'site']
+    [u'ommg', u'howr', u'visi', u'my', u'site', u'httpwwcoolstuffcom']
     """
     
     if not text: return 
-   
+    
     text = text.lower()
-
+    
     for e in settings.EMOTICONS:
         text = text.replace(e, '') #remove emoticons
-
+    
     format_pats = (
         #match, replace with
-        ("http.*", ''), #strip links
         ("@[A-Za-z0-9_]+", ''), #twitter specific ""
         ("#[A-Za-z0-9_]+", ''), # ""
         ("(\w)\\1{2,}", "\\1\\1"), #remove occurences of more than two consecutive repeating characters
+        ("<[^<]+?>", ''), #remove html tags
     )
     
     for pat in format_pats:
         text = re.sub(pat[0], pat[1], text)
-    
-    try:
-        text = str(''.join(BeautifulSoup(text).findAll(text=True))) #strip html and force str
-    except Exception, e:
-        print 'Exception occured:', e
-        return
-
-
-    text = text.translate(string.maketrans('', ''), string.punctuation).strip() #strip punctuation
-
+   
     if text:
+        text = text.translate(PUNC_MAP) #strip punctuation
+    
         words = [w for w in WhitespaceTokenizer().tokenize(text) if len(w) > 1]
     
         return words
