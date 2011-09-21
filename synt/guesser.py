469388b1e22496740c264ba4bb37305708ea543e
Titus Soporan <titus@tsoporan.com>
2011-09-21 19:23:24 -0400

    handle scores < -1 and > 1

--- synt/guesser.py
+++ synt/guesser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from synt.utils.redis_manager import RedisManager
 from synt.utils.extractors import best_word_feats
 from synt.utils.text import sanitize_text
@@ -12,10 +13,15 @@ def guess(text, classifier=DEFAULT_CLASSIFIER):
     
     bag_of_words = best_word_feats(sanitize_text(text))
     if bag_of_words:
-        guess = classifier.classify(bag_of_words)
+        
         prob = classifier.prob_classify(bag_of_words)
         
         #return a -1 .. 1 score
         score = prob.prob('positive') - prob.prob('negative')
+       
+        #if score doesn't fall within -1 and 1 return 0.0 
+        #example: single words might return a heavily biased score like -9.8343
+        if not (-1 <= score <= 1):
+            return 0.0
         
         return score
