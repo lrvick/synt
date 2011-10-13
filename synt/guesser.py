a947b18a9716482c913d74ab59582afdf2c327a4
Titus Soporan <titus@tsoporan.com>
2011-10-13 15:34:46 -0400

    move guess to Guesser class

--- synt/guesser.py
+++ synt/guesser.py
@@ -1,53 +1,62 @@
 # -*- coding: utf-8 -*-
-from synt.utils.redis_manager import RedisManager
+from synt.utils.db import RedisManager
 from synt.utils.extractors import WordExtractor, BestWordExtractor
 from synt.utils.text import sanitize_text
 from synt.logger import create_logger
 
 logger = create_logger(__file__)
 
-DEFAULT_CLASSIFIER = RedisManager().load_classifier()
-
-def guess(text, classifier=DEFAULT_CLASSIFIER, extractor=WordExtractor()):
-    """
-    Takes a blob of text and returns the sentiment score (-1.0 - 1.0).
+class Guesser(object):
     
-    Keyword Arguments:
-    classifier      -- the classifier to use 
-    extractor       -- the feature extractor to use utils.extractors
-    """
-    
-    if not classifier:
-        logger.error("guess needs a classifier")
-        return
-
-    tokens = sanitize_text(text)
-  
-    bag_of_words = extractor.extract(tokens)
-   
-    score = 0.0
-    
-    if bag_of_words:
+    def __init__(self, classifier='naivebayes', extractor=WordExtractor):
+        """
+        Initializes Guesser with defaults NaiveBayes classifier and WordExtractor extractor.
+        """
         
-        prob = classifier.prob_classify(bag_of_words)
+        self.classifier = RedisManager().load_classifier(classifier) 
+        self.extractor = extractor()
+        self.sanitizer = sanitize_text
+    
+    def guess(self, text):
+        """
+        Takes text and returns the sentiment score between -1 and 1.
+        """
         
-        #return a -1 .. 1 score
-        score = prob.prob('positive') - prob.prob('negative')
+        if not self.classifier:
+            logger.error("guess needs a classifier")
+            return
+
+        tokens = self.sanitizer(text)
+      
+        bag_of_words = self.extractor.extract(tokens)
        
-        #if score doesn't fall within -1 and 1 return 0.0 
-        if not (-1 <= score <= 1):
-            pass #score 0.0
+        score = 0.0
+        
+        if bag_of_words:
+            
+            prob = self.classifier.prob_classify(bag_of_words)
+            
+            #return a -1 .. 1 score
+            score = prob.prob('positive') - prob.prob('negative')
+           
+            #if score doesn't fall within -1 and 1 return 0.0 
+            if not (-1 <= score <= 1):
+                pass #score 0.0
 
-    return score
+        return score
+
+guess = Guesser()
 
 if __name__ == '__main__':
-    #example guess
+    #example usage of guess
+
+    g = Guesser()
+    
     print("Enter something to calucluate the synt of it!")
     print("Just press enter to quit.")
     
-    running = True
-    while running:
+    while True:
         text = raw_input("synt> ")
         if not text:
             break    
-        print('Guessed: {}'.format(guess(text)))
+        print('Guessed: {}'.format(g.guess(text)))
