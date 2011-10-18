f28a4125dd7d2a81c252db3df3c37dda6eb4d9c8
Titus Soporan <titus@tsoporan.com>
2011-10-18 17:43:13 -0400

    producer working with lists

--- synt/utils/processing.py
+++ synt/utils/processing.py
@@ -15,6 +15,8 @@ def batch_job(producer, consumer, chunksize=10000, processes=None):
     processes   -- how many processes should be created to handle jobs
     """
 
+    p = producer
+
     if type(producer) in [list,tuple]:
         #replace the list or tuple with a dummy producer function
         
@@ -23,8 +25,8 @@ def batch_job(producer, consumer, chunksize=10000, processes=None):
             A wraper for lists to allow them to be used as producers.
             """
             return producer[offset:offset+length]
-        producer = tmp
-
+        
+        p = tmp
 
     if not processes:
         processes = multiprocessing.cpu_count()
@@ -39,7 +41,7 @@ def batch_job(producer, consumer, chunksize=10000, processes=None):
         
         for i in range(1, processes + 1):
            
-            samples = producer(offset, chunksize)
+            samples = p(offset, chunksize)
             
             if not samples:
                 finished = True
@@ -55,11 +57,15 @@ def batch_job(producer, consumer, chunksize=10000, processes=None):
 
 if __name__=="__main__":
     #example usage
-    
+   
+
     def producer(offset, length):
         if offset >= 50:
             return []
         return range(offset, offset + length)
+
+    #or producer can be a list
+    #producer = range(100)
 
     queue = multiprocessing.Queue()
     def consumer(data):
